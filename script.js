c15fb1c0666306625cbbd54ab45789e6e2cef12e
nicky456 <sznici@gmail.com>
2021-10-19 17:41:35 +0200

    ..

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-function showCities(){
-    // let select = document.getElementById('select')
-    let list = document.querySelector('ul')
-    if(list.style.display == 'none'){
-        list.style.display = 'inline'
-    }else{
-        list.style.display = 'none'
-    }
+// function showCities(){
+//     // let select = document.getElementById('select')
+//     let list = document.querySelector('ul')
+//     if(list.style.display == 'none'){
+//         list.style.display = 'inline'
+//     }else{
+//         list.style.display = 'none'
+//     }
+// }
+
+var citySelector = document.getElementById("cities")
+var selectedCity = ""
+
+citySelector.addEventListener('input',updateCity)
+
+function updateCity(){
+    changeCity()
+    getData()
 }
 
+function changeCity(){
+    selectedCity = citySelector.value
+}
+changeCity()
+
 async function getData(){
-    let selectedCity = ""
-    document.querySelector('select').addEventListener("change",changeCity)
-    function changeCity(){
-        selectedCity = document.querySelector('select').value
-        console.log(selectedCity)
-        return selectedCity
-    }
-    console.log(changeCity())
-    let url = http://api.weatherapi.com/v1/current.json?key=54f66a90fbd841afa23121456211810&q=${changeCity()}&aqi=no%22
-    console.log(changeCity())
+    
+    let url = "http://api.weatherapi.com/v1/current.json?key=54f66a90fbd841afa23121456211810&q=" + selectedCity + "&aqi=no%22"
+    
     let response = await fetch(url)
     let parsedData = await response.json() //nyers adat
-    console.log(parsedData)
-
+    
     const city = document.getElementById('city')
     const tempature = document.getElementById('tempature')
     const sky = document.getElementById('skyCondition')
